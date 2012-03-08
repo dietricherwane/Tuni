7d567470ac758c18745f0611997b1eff484e6ab1
root <root@gmail.com>
2012-03-07 19:29:29 -0800

    changes

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tuni::Application.initialize!
-config.action_mailer.default_url_options = { :host => 'tuni.heroku.com' }

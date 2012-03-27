dc702e1d164124acdd30a75f84618d0c34b94215
root <root@gmail.com>
2012-03-26 18:25:13 -0700

    changes

--- app/controllers/casuals_controller.rb
+++ app/controllers/casuals_controller.rb
@@ -30,7 +30,7 @@ class CasualsController < ApplicationController
 		@workshop = params[:workshop_name]
 		
 		if (@firstname.eql?(nil) || @lastname.eql?(nil) || @identifier.eql?(nil) || @city_id.eql?(nil) || @company_id.eql?(nil) || @casual_type_id.eql?(nil) || @direction_id.eql?(nil) || @workshop.eql?("-Veuillez choisir un atelier-") || @workshop.eql?(nil))
-			redirect_to new_casual_path, :alert => "#{@firstname} #{@lastname} #{@birthdate_for_identifier} #{@identifier} #{@city_id} #{@company_id} #{@casual_type_id} #{@direction_id} #{@workshop}"
+			redirect_to new_casual_path, :alert => "Veuillez correctement remplir les champs."
 		else
 			Casual.create(:firstname => capitalization(@firstname), :lastname => capitalization(@lastname), :birthdate => @birthdate, :identifier => "#{@identifier+@birthdate_for_identifier+City.find_by_id(@city_id.to_i).short_name}", :city_id => @city_id.to_i, :company_id => @company_id.to_i, :casual_type_id => @casual_type_id.to_i, :workshop_id => Workshop.find_by_workshop_name(@workshop).id)
 			Casual.last.migration_dates.create(:entrance_date => Date.today.beginning_of_week)
